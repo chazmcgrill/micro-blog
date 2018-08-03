4be60f793310b9acf2c258390925a336aedfe837
chazmcgrill <charlietaylordev@gmail.com>
2018-08-03 12:25:06 +0100

    Created New User Route and Setup Express Parser

--- server/server.js
+++ server/server.js
@@ -5,9 +5,19 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/newuser', (req, res) => {
+  // check username / email for duplicates & validity
+  // get password hash
+  // add information to the database
+  // return success confirmation
+  res.json(req.body);
 });
 
 const port = process.env.PORT || 3000;
